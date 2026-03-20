cac0b8a1a9e14820d8d63196d27fef4bc9e06656
MEDIPC-047\mediopia <hybell.5448@gmail.com>
2026-03-20 11:08:47 +0900

    3/20-DB

---
+++ doc2/DDL/TB_LMS_ATFL_REPO_202603201056.sql
@@ -0,0 +1,10 @@
+INSERT INTO DEV_LMS.TB_LMS_ATFL_REPO (ATFL_REPO_ID,ATFL_REPO_PATH,ATFL_REPO_EXPLN,RGTR_ID,REG_DTTM,MDFR_ID,MOD_DTTM) VALUES
+	 ('MSG','/msg','메시지','system','20260101000000',NULL,NULL),
+	 ('BBS','/bbs','게시판','system','20260101000000',NULL,NULL),
+	 ('DSCS','/dscs','토론','system','20260101000000',NULL,NULL),
+	 ('ASMT','/asmt','과제','system','20260101000000',NULL,NULL),
+	 ('EXAM','/exam','시험','system','20260101000000',NULL,NULL),
+	 ('SMNR','/smnr','세미나','system','20260101000000',NULL,NULL),
+	 ('SRVY','/srvy','설문','system','20260101000000',NULL,NULL),
+	 ('CONTS','/conts','콘텐츠','system','20260101000000',NULL,NULL),
+	 ('USER','/user','사용자','system','20260101000000',NULL,NULL);
